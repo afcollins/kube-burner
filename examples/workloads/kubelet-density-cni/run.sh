4376c9390029afef03241e5354727ffe146b63cc
root <root@f33-h18-000-r640.rdu2.scalelab.redhat.com>
2026-09-01 00:38:22 +0000

    updates to run on a crc
    Signed-off-by: root <root@f33-h18-000-r640.rdu2.scalelab.redhat.com>

--- examples/workloads/kubelet-density-cni/run.sh
+++ examples/workloads/kubelet-density-cni/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-KUBE_BURNER=/root/git/kube-burner/bin/amd64/kube-burner
-$KUBE_BURNER init --set jobs.0.incrementalLoad.scrapeMetricsPerStep=true --set jobs.0.incrementalLoad.pattern.type=linear --set jobs.0.incrementalLoad.pattern.linear.stepSize=50 --set jobs.0.incrementalLoad.startIterations=600 --set jobs.0.incrementalLoad.totalIterations=900 -c kubelet-density-cni.yml -e mep.yml
+KUBE_BURNER=/root/go/bin/kube-burner
+$KUBE_BURNER init -e mep.yml -c kubelet-density-cni.yml \
+	--set jobs.0.incrementalLoad.scrapeMetricsPerStep=true \
+	--set jobs.0.incrementalLoad.startIterations=25 \
+	--set jobs.0.incrementalLoad.totalIterations=75 \
+	--set jobs.0.incrementalLoad.pattern.type=linear \
+	--set jobs.0.incrementalLoad.pattern.linear.stepSize=50
